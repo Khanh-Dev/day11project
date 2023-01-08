4677a302972e3d5638f2b998328b4ead1d4babe4
kit <1932004latoi@gmail.com>
2023-01-08 21:11:00 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class App extends Component {
+  state = {
+    firstName: '',
+    message: '',
+    key: '',
+  }
 
-const App = () => {
-  return (
-    <h1>OK test</h1>
-  )
+  handleOnclick = (e) => {
+    //e gives an event object
+    //check the value of e using console.log(e)
+    this.setState({message: 'Welcome to the world of events'})
+  }
+
+  handleMouseMove = (e) => {
+    this.setState({message: 'mouse is moving'})
+  }
+
+  handleChange = (e) => {
+    this.setState({firstName: e.target.value, message: e.target.value})
+  }
+
+  handleKeyPress = (e) => {
+    this.setState({message: `${e.target.value} has been pressed and the keycode is ` + e.charCode})
+  }
+
+  handleBlur = (e) => {
+    this.setState({message: 'Input field has been blurred'})
+  }
+
+  handleCopy = (e) => {
+    this.setState({message: "Text don't allowed Copy"})
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <h1>Welcome to the world of Events</h1>
+
+        <button onClick={this.handleOnclick}>Click Me</button>
+        <button onClick={this.handleMouseMove}>Move mouse on me</button>
+        <p onCopy={this.handleCopy}>Check this text</p>
+        <p>{this.state.message}</p>
+      </div>
+    )
+    
+  }
 }
+
 
 const root = document.getElementById('root')
 ReactDOM.render(<App />, root)
